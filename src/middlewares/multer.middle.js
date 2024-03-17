611cac3aceb3393a1462e0b7504bbe41ba4f4f2d
gurshaagrawal1211 <gurshaagrawal@gmail.com>
2024-03-17 15:00:02 +0530

    file upload using cloudinary and multer

---
+++ src/middlewares/multer.middle.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname)
+    }
+  })
+  
+export const upload = multer({ 
+    storage, 
+})
+
+//we can get all the json data that is given by the user by 'req' but to get file we uses multer
