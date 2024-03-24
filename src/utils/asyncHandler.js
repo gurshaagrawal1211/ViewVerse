bed7e46fbdea83e8457e85302fced408ca37ab20
gurshaagrawal1211 <gurshaagrawal@gmail.com>
2024-03-24 17:59:47 +0530

    userController

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,15 +1,15 @@
-// const asyncHandler = () => {}
-// const asyncHandler = (func) => () => {}
-// const asyncHandler = (func) => async () => {}
-
 const asyncHandler = (requestHandler) => {
     return (req,res,next) => {
-        Promise.resolve(requestHandler(res,req,next))
+        Promise.resolve(requestHandler(req,res,next))
         .catch((err) => next(err))
     }
 }
 
 export {asyncHandler}
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
 
 // const asyncHandler = (fn) = async(req,res,next) => {
 //     try {
